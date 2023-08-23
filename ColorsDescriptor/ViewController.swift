af12a1a504f47ed16dec86e96b9633fece1953f1
Belal Hmeidat <belalhmeidat@icloud.com>
2023-08-23 12:13:55 +0300

    Initial Commit

---
+++ ColorsDescriptor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorsDescriptor
+//
+//  Created by Belal Hmeidat on 8/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
