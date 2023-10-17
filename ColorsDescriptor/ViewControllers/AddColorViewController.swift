9e120cc01835f270b282cda583f3973224ac85d2
Belal Hmeidat <belalhmeidat@icloud.com>
2023-10-17 13:55:55 +0300

    Toolbar animation, method extraction, private properties, extentions in one file, code clean up, documentation, textfield border, textfield padding

--- ColorsDescriptor/ViewControllers/AddColorViewController.swift
+++ ColorsDescriptor/ViewControllers/AddColorViewController.swift
@@ -12,35 +12,42 @@ protocol AddColorViewControllerDelegate : AnyObject {
 }
 
 class AddColorViewController: UIViewController, UIColorPickerViewControllerDelegate {
-
-    //MARK: Outlets
-    @IBOutlet weak var colorTitleTextField: UITextField!
-    @IBOutlet weak var colorDescTextView: UITextView!
-    @IBOutlet weak var chooseColorBt: UIButton!
     
-    //MARK: Global
-    var selectedColor : UIColor?
-    var colorTitle : String?
-    var colorDesc : String?
+    //MARK: Outlets
+    @IBOutlet private weak var colorTitleTextField: UITextField!
+    @IBOutlet private weak var colorDescTextView: UITextView!
+    @IBOutlet private weak var chooseColorBt: UIButton!
     
     //MARK: Delegate
     weak var delegate : (AddColorViewControllerDelegate)?
     
     //MARK: Function
-    fileprivate func setupBorders() {
-        colorDescTextView.layer.borderColor = UIColor.lightGray.cgColor
-        colorDescTextView.layer.borderWidth = 1
-        colorDescTextView.layer.cornerRadius = 20
-        colorTitleTextField.clipsToBounds = true
-        colorTitleTextField.layer.cornerRadius = 13
-        colorDescTextView.layer.borderWidth = 1
-        colorTitleTextField.layer.borderColor = UIColor.lightGray.cgColor
-        colorDescTextView.textContainerInset = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 5)
+    fileprivate func addPaddingToTextfield() {
+        let paddingView = UIView(frame: CGRectMake(0, 0, 15, self.colorTitleTextField.frame.height))
+        colorTitleTextField.leftView = paddingView
+        colorTitleTextField.leftViewMode = UITextField.ViewMode.always
+        colorTitleTextField.rightView = paddingView
+        colorTitleTextField.rightViewMode = UITextField.ViewMode.always
+    }
+    
+    private func setupBorders() {
+        
+    ///Title Textfield Setup
+    colorTitleTextField.clipsToBounds = true ///Needed for the rounded corners
+    colorTitleTextField.layer.cornerRadius = 13
+    colorTitleTextField.layer.borderWidth = 1
+    colorTitleTextField.layer.borderColor = UIColor.lightGray.cgColor
+    addPaddingToTextfield()
+
+    ///Description Textview Setup
+    colorDescTextView.layer.borderColor = UIColor.lightGray.cgColor
+    colorDescTextView.layer.borderWidth = 1
+    colorDescTextView.layer.cornerRadius = 20
+    colorDescTextView.textContainerInset = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 5)
     }
     /// When the color is selected, the color button add view is updated to match the selected color
     func colorPickerViewController(_ viewController: UIColorPickerViewController, didSelect color: UIColor, continuously: Bool) {
-        selectedColor = color
-        chooseColorBt.tintColor = selectedColor
+        chooseColorBt.tintColor = color
     }
     
     //MARK: Initilization
@@ -50,32 +57,40 @@ class AddColorViewController: UIViewController, UIColorPickerViewControllerDeleg
     }
     
     //MARK: Outlet functions
+    private func showAlert(_ error: String?) {
+        let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert,animated: true)
+    }
+    
     /// validates the input information of the color to be added and adds it to the list of color then saves it to the context of data core
     /// Closes of the view and returns back to the colors view
-    @IBAction func addColorPressed(_ sender: UIButton) {
-        colorTitle = colorTitleTextField.text!
-        colorDesc = colorDescTextView.text!
-        let error = Validator.isColorValid(title: colorTitle!, description: colorDesc!, color: selectedColor ?? chooseColorBt.tintColor)
-        if error != nil {
-            let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert,animated: true)
+    @IBAction private func addColorPressed(_ sender: UIButton) {
+        let colorTitle = colorTitleTextField.text!
+        let colorDesc = colorDescTextView.text!
+        let alertMessage = Validator.isColorValid(title: colorTitle, description: colorDesc, color: chooseColorBt.tintColor)
+        if alertMessage != nil {
+            showAlert(alertMessage)
             return
         }
-        ColorElement(color: selectedColor ?? chooseColorBt.tintColor, name: colorTitle!, description: colorDesc!)
+        ColorElement(color: chooseColorBt.tintColor, name: colorTitle, description: colorDesc)
         ColorManager.shared.saveColorList()
         self.dismiss(animated: true, completion: {self.delegate?.actionRequiresReloadPerformed()}) //closing view controller
     }
     
-    /// Pressing the color button shows a color picker view
-    @IBAction func colorButtonPressed(_ sender: UIButton) {
+    private func openColorPickerView() {
         let colorPickerViewController = UIColorPickerViewController()
         colorPickerViewController.delegate = self
         colorPickerViewController.supportsAlpha = false /// no opacity for colors
         present(colorPickerViewController, animated: true)
     }
+    
+    /// Pressing the color button shows a color picker view
+    @IBAction private func colorButtonPressed(_ sender: UIButton) {
+        openColorPickerView()
+    }
     /// Dismisses the add color view and returns to the colors view
-    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+    @IBAction private func cancelButtonPressed(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
 }
--- ColorsDescriptor/ViewControllers/ColorsViewController.swift
+++ ColorsDescriptor/ViewControllers/ColorsViewController.swift
@@ -11,35 +11,19 @@ import CoreData
 class ColorsViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var editButton: UIBarButtonItem!
-    @IBOutlet private weak var tableView: UITableView!
-    @IBOutlet private var colorDetailsView: UIView!
-    @IBOutlet private var descriptionLb: UILabel!
+    @IBOutlet private(set) weak var tableView: UITableView!
+    @IBOutlet private(set) var colorDetailsView: UIView!
+    @IBOutlet private(set) var descriptionLb: UILabel!
     @IBOutlet weak var toolbar: UIToolbar!
     @IBOutlet weak var trashColorButton: UIBarButtonItem!
     
     static var globalTableView : ColorsViewController?
-//    var container: NSPersistentContainer!
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "ColorsListData")
-        container.loadPersistentStores(completionHandler: {(storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }})
-        return container
-    }()
     
     //MARK: constants
-    private let cellIdentifier = "ColorTableViewCell"
+    let cellIdentifier = "ColorTableViewCell"
     
 
     //MARK: initilizing view
-    private func setupView(){
-        title = "Colors"
-        colorDetailsView.backgroundColor = ColorManager.shared.defaultColor
-        tableView.register(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
-        toolbar.isHidden = true
-        trashColorButton.isEnabled = false
-    }
     
     override func viewDidLoad() {
         ColorsViewController.globalTableView = self
@@ -47,6 +31,14 @@ class ColorsViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    private func setupView(){
+        title = "Colors"
+        colorDetailsView.backgroundColor = ColorManager.shared.defaultColor
+        tableView.register(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
+//        toolbar.isHidden = true
+        trashColorButton.isEnabled = false
+    }
+        
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         if segue.identifier == "AddColorSegue" {
            let detailVC = segue.destination as! AddColorViewController
@@ -55,27 +47,27 @@ class ColorsViewController: UIViewController {
     }
     
     // Function to animate the toolbar sliding up or down
-    func toggleToolbarVisibility() {
-        let toolbarHeight = self.toolbar.frame.size.height
-        let yOffset: CGFloat = toolbar.isHidden ? -toolbarHeight : toolbarHeight
-
+    func toggleToolbarVisibility(show : Bool) {
+        /// Added 10 so toolbar buttons don't get clipped at corners
+        let toolbarHeight = -1 * (self.toolbar.frame.size.height + 10)
+        let yOffset: CGFloat = !show ? 0.0 : toolbarHeight
         UIView.animate(withDuration: 0.2) {
-            self.toolbar.frame = self.toolbar.frame.offsetBy(dx: 0, dy: yOffset)
+            self.toolbar.transform = CGAffineTransform(translationX: 0, y: yOffset)
+            self.view.layoutIfNeeded()
         }
-        toolbar.isHidden.toggle()
     }
     /// updates the color so that the removed color detail is never shown afer deletion
-    fileprivate func updateSelectedColor() {
+    private func updateSelectedColor() {
         colorDetailsView.backgroundColor = UIColor(value: Int(ColorManager.shared.colorElements[0].value))
         descriptionLb.text = ColorManager.shared.colorElements[0].desc
     }
     
     //MARK: Button Actions
-    @IBAction func editButtonAction(_ sender: UIBarButtonItem) {
+    @IBAction private func editButtonAction(_ sender: UIBarButtonItem) {
         /// Toggles editing mode for the table
         tableView.isEditing.toggle()
         /// Enables the toolbar for delete and add while in edit mode
-        toggleToolbarVisibility()
+        toggleToolbarVisibility(show: tableView.isEditing)
         if (tableView.isEditing == true){
             editButton.title = "Done"
         }
@@ -88,7 +80,7 @@ class ColorsViewController: UIViewController {
         tableView.reloadData()
     }
     
-    @IBAction func deleteButtonAction(_ sender: UIBarItem) {
+    @IBAction private func deleteButtonAction(_ sender: UIBarItem) {
         ColorManager.shared.deleteSelectedColors()
         /// Updates the selected color for the detail view to the color of the first element when a color is deleted
         updateSelectedColor()
@@ -102,64 +94,3 @@ class ColorsViewController: UIViewController {
         trashColorButton.isEnabled = ColorManager.shared.checkDelete()
     }
 }
-
-// MARK: - Table View Delegate, Data Source, and functionalites
-extension ColorsViewController : UITableViewDelegate, UITableViewDataSource {
-    /// setting the number of rows
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        ColorManager.shared.colorElements.count
-       /// number of rows depend on the number of colors in the colors controller
-   }
-    
-    //MARK: cell content configuration
-   /// setting contents for each row (cell bg color and cell color name)
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? ColorTableViewCell else{
-           return UITableViewCell()
-       }
-        /// storing the bg color of the cell to be passed when building it
-        let color =  ColorManager.shared.colorElements[indexPath.row]
-        /// passing cell build parameters
-        cell.setup(with: color, index:indexPath.row, editing: self.tableView.isEditing)
-       return cell
-        
-   }
-    //MARK: cell edit configuration
-    ///reference: https://www.ralfebert.com/ios-examples/uikit/uitableviewcontroller/reorderable-cells/
-    ///enabling moving cells
-    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        true
-    }
-    
-    /// reflecting changes to the list of color elements as a result of moving cell
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        ColorManager.shared.moveElement(sourceIndexPath, destinationIndexPath)
-        tableView.reloadData()
-    }
-    
-    /// Disabling editing style becuase non is used
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .none
-    }
-    
-    /// disabling Identation because it's done manually through ColorTableViewCell
-    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-    
-    //MARK: cell action configuration
-    /// Action to be performed when a table cell is clicked
-    /// Sets the bottom view backgroud color to the color of the cell and shows a description of that color
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        colorDetailsView.backgroundColor =  UIColor(value: Int(ColorManager.shared.colorElements[indexPath.row].value))
-        descriptionLb.text =  ColorManager.shared.colorElements[indexPath.row].desc
-    }
-}
-
-/// delegate extension to refresh the table data after adding a color action is performed in the AddColoViewController
-extension ColorsViewController : AddColorViewControllerDelegate {
-    func actionRequiresReloadPerformed() {
-        tableView.reloadData()
-    }
-}
-
